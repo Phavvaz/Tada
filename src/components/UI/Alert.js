99a0ac9ab32e78551b4b99c3e87e1d45259f4673
Phavvaz <phavvaz506@gmail.com>
2020-06-28 11:18:20 -0500

    successfully altered

--- src/components/UI/Alert.js
+++ src/components/UI/Alert.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {Row, Col, Alert} from 'reactstrap';
+import { Row, Col, Alert } from 'reactstrap';
 
 const Alart = (props) => {
-
-// const AlertTimeOut = () => setTimeout(() => console.log('success'), 1000);
-
-  return(
+  return (
     <Row>
       <Col>
-         <Alert color={props.color} fade>
-            {props.children}
-         </Alert>
+        <Alert color={props.color} fade>
+          {props.children}
+        </Alert>
       </Col>
     </Row>
   );
